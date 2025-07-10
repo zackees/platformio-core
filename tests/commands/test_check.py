669fb7db09ad505aadb97ab5de33b2c138eeee5e
zackees <z@zackees.com>
2025-07-10 14:53:55 -0700

    update

--- tests/commands/test_check.py
+++ tests/commands/test_check.py
@@ -512,7 +512,7 @@ TEST-TEST-TEST-TEST
     assert "license information is incorrect" in verbose_result.output.lower()
 
 
-@pytest.mark.skip(reason="Test takes too long to run")
+# @pytest.mark.skip(reason="Test takes too long to run")
 @pytest.mark.parametrize("framework", ["arduino", "stm32cube", "zephyr"])
 @pytest.mark.parametrize("check_tool", ["cppcheck", "clangtidy", "pvs-studio"])
 def test_check_embedded_platform_all_tools(
